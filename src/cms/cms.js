8e4b912cb829e554e56bd21ed9a86ba0f118a72b
Devon Wolfkiel <devon.wolfkiel@cdw.com>
2023-01-25 17:04:10 -0800

    add log

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -7,6 +7,8 @@ import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
+console.log("cms.js loaded!");
+
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 
