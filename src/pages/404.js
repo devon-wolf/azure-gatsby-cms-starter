a20ceae0968fcab7c8e66a938d8a42caa73e8743
Devon Wolfkiel <devon.wolfkiel@cdw.com>
2023-01-27 11:52:11 -0800

    prune much more aggressively, update readme

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,26 @@
-import * as React from 'react'
+import React from 'react'
+
+import YMLContent from '../content/404.yml'
 import Layout from '../components/Layout'
 
 const NotFoundPage = () => (
   <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
+    <section className="section section--gradient">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <div className="section">
+              <h1 className="title is-size-3 has-text-weight-bold is-bold-light">
+                {YMLContent.title}
+              </h1>
+              <div className="content">
+                <p>{YMLContent.content}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </Layout>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
-import YMLContent from './index.yml'
-import Content from '../components/Content'
+import YMLContent from '../content/index.yml'
 import Layout from '../components/Layout'
 
 const IndexPage = () => {
@@ -15,10 +14,9 @@ const IndexPage = () => {
                 <h1 className="title is-size-3 has-text-weight-bold is-bold-light">
                   {YMLContent.title}
                 </h1>
-                <Content
-                  className="content"
-                  content={<p>{YMLContent.content}</p>}
-                />
+                <div className="content">
+                  <p>{YMLContent.content}</p>
+                </div>
               </div>
             </div>
           </div>
